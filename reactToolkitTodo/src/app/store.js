32d13b3b9eb375ea4ce523fe6ec369b2a41f8899
Dineshpokhrel <dineshpokhrel001@gmail.com>
2024-06-22 21:29:24 +0545

    learned about redux toolkit and made todo app to understand it better

--- reactToolkitTodo/src/app/store.js
+++ reactToolkitTodo/src/app/store.js
@@ -4,3 +4,7 @@ import todoReducer from '../features/todo/todoSlice'
 export const store = configureStore({
     reducer: todoReducer
 })
+
+
+//every application single store which we called single soure of truth
+
